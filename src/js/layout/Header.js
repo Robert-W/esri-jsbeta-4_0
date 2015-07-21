cede3710f38729600e85859c631dba2a8fd39d6c
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-07-21 12:44:05 -0400

    removed unnecessary constructor in react components

--- src/js/layout/Header.js
+++ src/js/layout/Header.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 export class Header extends React.Component {
 
-  constructor (props) {
-    super(props);
-  }
-
   render () {
     return (
       <header className='app-header'>
--- src/js/layout/MainPanel.js
+++ src/js/layout/MainPanel.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 export class MainPanel extends React.Component {
 
-  constructor (props) {
-    super(props);
-  }
-
   render () {
     return (
       <div className='app-body'>
